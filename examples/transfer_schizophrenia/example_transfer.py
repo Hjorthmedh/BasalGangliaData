6a57a629fc99de69770d20a3bbc6283173e80c0a
Johannes Hjorth <hjorth@kth.se>
2024-10-01 10:17:00 +0200

    Updated transfer

--- examples/transfer_schizophrenia/example_transfer.py
+++
@@ -1,5 +0,0 @@
-# WT files
-python ../simple_transfer.py -s /home/hjorth/HBP/bgmod/models/optim/schizophrenic/WT -d /home/hjorth/HBP/bgmod/models/optim/schizophrenic/BGDATA/WT/neurons/striatum -a
-
-# Schizophrenia
-python ../simple_transfer.py -s /home/hjorth/HBP/bgmod/models/optim/schizophrenic/d2oe -d /home/hjorth/HBP/bgmod/models/optim/schizophrenic/BGDATA/d2oe/neurons/striatum -a
---
+++ examples/transfer_schizophrenia/transfer_schizofrenia.sh
@@ -0,0 +1,5 @@
+# WT files
+python ../simple_transfer.py -s ../../../bgmod/models/optim/schizophrenic/WT -d ../../../bgmod/models/optim/schizophrenic/BGDATA/WT/neurons/striatum -a
+
+# Schizophrenia
+python ../simple_transfer.py -s ../../../bgmod/models/optim/schizophrenic/d2oe -d ../../../bgmod/models/optim/schizophrenic/BGDATA/d2oe/neurons/striatum -a
