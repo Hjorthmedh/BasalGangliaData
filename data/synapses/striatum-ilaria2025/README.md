54c5f72119b340deaab96836995cca772a98eeb2
Johannes Hjorth <hjorth@kth.se>
2025-11-03 08:48:41 +0100

    Added Ilarias JSON files

---
+++ data/synapses/striatum-ilaria2025/README.md
@@ -0,0 +1,6 @@
+Data files from:
+https://github.com/IlaCar/PSC_double_decay_fitting/tree/main/model/json_files
+
+Backup fork:
+https://github.com/Hjorthmedh/PSC_double_decay_fitting/tree/main/model/json_files
+
--- tools/calculate_synapse_density.py
+++ tools/calculate_synapse_density.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import json
 from snudda import SnuddaLoad
 
 def process_file(path):
@@ -10,7 +11,15 @@ def process_file(path):
 
     incoming_synapses_name = {}
     incoming_synapses_morph = {}
+    incoming_synapse_density = {}
 
+    with open("/home/hjorth/HBP/szmod/analysis/metadata/dspn_d2oe_0.00.json", "r") as f:
+        morph_info = json.load(f)
+
+    with open("/home/hjorth/HBP/szmod/analysis/metadata/ispn_d2oe_0.00.json", "r") as f:
+        morph_info |= json.load(f)
+
+    
     for neuron_name in np.unique([x["name"] for x in sl.data["neurons"]]):
         print(f"Processing {neuron_name}")
     
@@ -23,6 +32,8 @@ def process_file(path):
             name = sl.data["neurons"][neuron_id]["name"]
             morph = sl.data["neurons"][neuron_id]["morphology"]
 
+            morph_stub = os.path.splitext(os.path.basename(morph))[0]
+            
             if name not in incoming_synapses_name:
                 incoming_synapses_name[name] = []
 
@@ -34,14 +45,42 @@ def process_file(path):
             incoming_synapses_morph[morph].append(n_incoming)
 
 
-    for name, data in incoming_synapses_name.items():
-        print(f"{name}: {np.mean(data)} +/ {np.std(data)}")
+            if morph_stub in morph_info:
+                if morph_stub not in incoming_synapse_density:
+                    incoming_synapse_density[morph_stub] = []
 
-    for morph, data in incoming_synapses_morph.items():
-        print(f"{os.path.basename(morph)}: {np.mean(data)} +/ {np.std(data)}")
+                dend_len = morph_info[morph_stub]["dend"]["length"]
+                
+                incoming_synapse_density[morph_stub].append(n_incoming/dend_len)
 
         
 
+    for name, data in incoming_synapses_name.items():
+        print(f"{name}: {np.mean(data):.2f} +/ {np.std(data):.2f}")
+
+    for morph, data in incoming_synapses_morph.items():
+        print(f"{os.path.basename(morph)}: {np.mean(data):.2f} +/ {np.std(data):.2f}")
+
+    len_list = []
+    den_list = []
+    
+        
+    for morph, data in incoming_synapse_density.items():
+        morph_stub = os.path.splitext(morph)[0]
+        print(f"Density of {morph} (with dend length {morph_info[morph_stub]['dend']['length']:.2f} micrometer): {np.mean(data):.4f} +/- {np.std(data):.4f} synapses/micrometer")
+        len_list.append(morph_info[morph_stub]['dend']['length'])
+        den_list.append(np.mean(data))
+
+
+    import matplotlib.pyplot as plt
+    plt.figure()
+    plt.plot(len_list,den_list,'k.')
+    plt.xlabel("Dendritic length (micrometer)")
+    plt.ylabel("Synapse density (1/micrometer)")
+    plt.savefig("spn_synapse_density_summary.png")
+    plt.ion()
+    plt.show()
+        
             
     
     import pdb
@@ -50,4 +89,14 @@ def process_file(path):
 
 if __name__ == "__main__":
 
-    process_file("/home/hjorth/HBP/Snudda/examples/networks/test-10k/network-synapses.hdf5")
+    # process_file("/home/hjorth/HBP/Snudda/examples/networks/test-10k/network-synapses.hdf5")
+
+    # process_file("/home/hjorth/HBP/Snudda/examples/networks/test-10k-mu2_zero/network-synapses.hdf5")
+
+    # process_file("/home/hjorth/HBP/Snudda/examples/networks/test-10k-mu2_one/network-synapses.hdf5")
+
+    # rocess_file("/home/hjorth/HBP/Snudda/examples/networks/test-10k-mu2_one_sm3/network-synapses.hdf5")
+
+    # process_file("/home/hjorth/HBP/Snudda/examples/networks/test-10k-mu2_1.5/network-synapses.hdf5")
+
+    process_file("/home/hjorth/HBP/Snudda/examples/networks/test-10k-mu2_2/network-synapses.hdf5")        
